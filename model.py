3babfb6139423f69052dff3849162c6095313ff3
ilay menahem <110230889+IlayMenahem@users.noreply.github.com>
2025-01-29 16:50:43 +0200

    larger model

--- model.py
+++ model.py
@@ -33,21 +33,24 @@ class mnist_model(eqx.Module):
 class mnist_feature_extractor(eqx.Module):
     conv1: eqx.nn.Conv2d
     conv2: eqx.nn.Conv2d
+    conv3: eqx.nn.Conv2d
     linear1: eqx.nn.Linear
     linear2: eqx.nn.Linear
 
     def __init__(self, key):
-        key1, key2, key3, key4 = jax.random.split(key, 4)
+        key1, key2, key3, key4, key5 = jax.random.split(key, 5)
 
-        self.conv1 = eqx.nn.Conv2d(1, 32, 3, 1, padding=1, key=key1)
-        self.conv2 = eqx.nn.Conv2d(32, 64, 3, 1, padding=1, key=key2)
-        self.linear1 = eqx.nn.Linear(64*(image_dim-1)**2, 128, key=key3)
-        self.linear2 = eqx.nn.Linear(128, 128, key=key4)
+        self.conv1 = eqx.nn.Conv2d(1, 16, 3, 1, padding=1, key=key1)
+        self.conv2 = eqx.nn.Conv2d(16, 32, 3, 1, padding=1, key=key2)
+        self.conv3 = eqx.nn.Conv2d(32, 64, 3, 1, padding=1, key=key3)
+        self.linear1 = eqx.nn.Linear(64*(image_dim-1)**2, 128, key=key4)
+        self.linear2 = eqx.nn.Linear(128, 128, key=key5)
 
     @jax.jit
     def __call__(self, x):
         x = jax.nn.relu(self.conv1(x))
         x = jax.nn.relu(self.conv2(x))
+        x = jax.nn.relu(self.conv3(x))
         x = eqx.nn.MaxPool2d(kernel_size=2)(x)
         x = jnp.ravel(x)
         x = jax.nn.relu(self.linear1(x))
@@ -60,14 +63,16 @@ class mnist_generator(eqx.Module):
     linear2: eqx.nn.Linear
     conv1: eqx.nn.ConvTranspose2d
     conv2: eqx.nn.ConvTranspose2d
+    conv3: eqx.nn.ConvTranspose2d
 
     def __init__(self, key):
-        key1, key2, key3, key4 = jax.random.split(key, 4)
+        key1, key2, key3, key4, key5 = jax.random.split(key, 5)
 
         self.linear1 = eqx.nn.Linear(128, 128, key=key1)
         self.linear2 = eqx.nn.Linear(128, 64*image_dim**2, key=key2)
         self.conv1 = eqx.nn.ConvTranspose2d(64, 32, 3, 1, padding=1, key=key3)
-        self.conv2 = eqx.nn.ConvTranspose2d(32, 1, 3, 1, padding=1, key=key4)
+        self.conv2 = eqx.nn.ConvTranspose2d(32, 16, 3, 1, padding=1, key=key4)
+        self.conv3 = eqx.nn.ConvTranspose2d(16, 1, 3, 1, padding=1, key=key5)
 
     @jax.jit
     def __call__(self, x):
@@ -75,7 +80,8 @@ class mnist_generator(eqx.Module):
         x = jax.nn.relu(self.linear2(x))
         x = x.reshape((64, image_dim, image_dim))
         x = jax.nn.relu(self.conv1(x))
-        x = jax.nn.sigmoid(self.conv2(x))
+        x = jax.nn.relu(self.conv2(x))
+        x = jax.nn.sigmoid(self.conv3(x))
 
         return x
 
@@ -86,7 +92,7 @@ class mnist_diffusion(eqx.Module):
     time_embedding: eqx.nn.Embedding
 
     def __init__(self, key):
-        key1, key2, key3, key4 = jax.random.split(key, 4)
+        key1, key2, key3, key4, key5 = jax.random.split(key, 5)
 
         self.feature_extractor = mnist_feature_extractor(key1)
         self.label_embedding = eqx.nn.Embedding(10, 128, key=key2)
